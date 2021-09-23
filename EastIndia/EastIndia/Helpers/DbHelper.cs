02e21aae2d549ec38fa3586a2605abd1821b5ccd
Mateusz Król <krol@netcompany.com>
2021-09-23 15:51:51 +0200

    Add generic database helper

---
+++ EastIndia/EastIndia/Helpers/DbHelper.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using EastIndia.Models;
+
+namespace EastIndia.Helpers
+{
+	public class DbHelper
+	{
+		private static readonly EastIndiaEntities entities = new EastIndiaEntities();
+
+		public static bool Insert<T>(T entity) where T : class
+		{
+			try
+			{
+				entities.Set<T>().Add(entity);
+				entities.SaveChanges();
+				return true;
+			}
+			catch (Exception e)
+			{
+				ErrorLogger.LogError(e);
+				return false;
+			}
+		}
+
+		public static bool Update<T>(Guid oldEntityId, T newEntityValues) where T : class
+		{
+			try
+			{
+				var oldEntity = entities.Set<T>().Find(oldEntityId);
+				entities.Entry(oldEntity).CurrentValues.SetValues(newEntityValues);
+				entities.SaveChanges();
+				return true;
+			}
+			catch (Exception e)
+			{
+				ErrorLogger.LogError(e);
+				return false;
+			}
+		}
+
+		public static bool Remove<T>(Guid id) where T : class
+		{
+			try
+			{
+				var set = entities.Set<T>();
+				var entity = set.Find(id);
+				set.Remove(entity);
+				entities.SaveChanges();
+				return true;
+			}
+			catch (Exception e)
+			{
+				ErrorLogger.LogError(e);
+				return false;
+			}
+		}
+	}
+}
