d83c27a63d2a69d964099de672fa51d87ce0b9fd
Piotr Kałwa <piok@netcompany.com>
2021-09-23 23:14:35 +0200

    Integrations working

--- EastIndia/EastIndia/Integrations/IntegrationService.cs
+++ EastIndia/EastIndia/Integrations/IntegrationService.cs
@@ -1,27 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
 using Newtonsoft.Json;
 using EastIndia.Models;
 using EastIndia.Models.Dtos;
-using Flurl.Http;
-using Package = EastIndia.Models.Dtos.Package;
 
 namespace EastIndia.Integrations
 {
     public class IntegrationService
     {
-        public List<ExternalRouteDetails> GetAllRoutes(Package package, Vendor vendor)
+        public List<ExternalRouteDetails> GetAllRoutes(ExternalPackage package, Vendor vendor)
         {
             try
             {
-                var uri = vendor == Vendor.TelstarLogistics ? 
-                    "http://wa-eit-dk1.azurewebsites.net/api/getAllRoutes" :
-                    "http://wa-oa-dk1.azurewebsites.net/api/integration";
+                var handler = new WinHttpHandler();
+                var client = new HttpClient(handler);
                 
-                var json = JsonConvert.SerializeObject(package);
-                var res = uri.PostJsonAsync(json).Result;
+                var uri = vendor == Vendor.TelstarLogistics ?
+                    "https://wa2-tl-dk1.azurewebsites.net/api/getRoutesInfo" :
+                    "http://wa-oa-dk1.azurewebsites.net/api/integration";
 
-                return res.GetJsonAsync<List<ExternalRouteDetails>>().Result;
+                var json = JsonConvert.SerializeObject(package);
+                var request = new HttpRequestMessage
+                {
+                    Method = vendor == Vendor.TelstarLogistics ? HttpMethod.Post : HttpMethod.Get,
+                    RequestUri = new Uri(uri),
+                    Content = new StringContent(json, Encoding.UTF8, "application/json"),
+                };
+
+                var response = client.SendAsync(request).Result;
+                response.EnsureSuccessStatusCode();
+
+                var content = response.Content.ReadAsStringAsync().Result;
+                return JsonConvert.DeserializeObject<List<ExternalRouteDetails>>(content);
             }
             catch (Exception ex)
             {
