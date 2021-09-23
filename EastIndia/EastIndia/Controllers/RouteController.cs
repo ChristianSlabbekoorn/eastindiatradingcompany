b4a647d5997a5d20f5c9108b961fc411b7eb5c6c
Mateusz Król <krol@netcompany.com>
2021-09-23 22:05:07 +0200

    Add ChangePrice unit tests

--- EastIndia/EastIndia/Controllers/RouteController.cs
+++ EastIndia/EastIndia/Controllers/RouteController.cs
@@ -2,6 +2,7 @@
 using System.Web.Http;
 using System.Web.Mvc;
 
+using EastIndia.Helpers;
 using EastIndia.Managers;
 using EastIndia.Models.Dtos;
 
@@ -21,7 +22,8 @@ namespace EastIndia.Controllers
 
         public ActionResult ChangePrice([FromBody] PriceUpdate price)
         {
-            return PriceManager.UpdatePrice(price) ?
+            var manager = new PriceManager(new DbHelper());
+            return manager.UpdatePrice(price) ?
                 new HttpStatusCodeResult(HttpStatusCode.OK) :
                 new HttpStatusCodeResult(HttpStatusCode.InternalServerError);
         }
