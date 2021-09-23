c09de159c9e5840d92e26926157217afc4f5e84c
Christian Slabbekoorn <chsl@netcompany.com>
2021-09-23 21:03:07 +0200

    Solved some merge issues

---
+++ EastIndia/EastIndia/Helpers/DbHelper.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using EastIndia.Models;
+
+namespace EastIndia.Helpers
+{
+	public class DbHelper
+	{
+		private static readonly Entities entities = new Entities();
+
+		public static T Get<T>(Guid id) where T : class
+		{
+			return entities.Set<T>().Find(id);
+		}
+
+		public static bool Insert<T>(T entity) where T : class
+		{
+			try
+			{
+				entities.Set<T>().Add(entity);
+				entities.SaveChanges();
+				return true;
+			}
+			catch (Exception e)
+			{
+				ErrorLogger.LogError(e);
+				return false;
+			}
+		}
+
+		public static bool Update<T>(Guid oldEntityId, T newEntityValues) where T : class
+		{
+			try
+			{
+				var oldEntity = entities.Set<T>().Find(oldEntityId);
+				entities.Entry(oldEntity).CurrentValues.SetValues(newEntityValues);
+				entities.SaveChanges();
+				return true;
+			}
+			catch (Exception e)
+			{
+				ErrorLogger.LogError(e);
+				return false;
+			}
+		}
+
+		public static bool Remove<T>(Guid id) where T : class
+		{
+			try
+			{
+				var set = entities.Set<T>();
+				var entity = set.Find(id);
+				set.Remove(entity);
+				entities.SaveChanges();
+				return true;
+			}
+			catch (Exception e)
+			{
+				ErrorLogger.LogError(e);
+				return false;
+			}
+		}
+	}
+}
